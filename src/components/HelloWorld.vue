a6219c96ea8730564aa460a968dbb7fb3f229950
loveaoe33 <loveaoe33@gmail.com>
2022-10-13 17:09:49 +0800

    fixUrl

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1000,7 +1000,7 @@ export default {
     msg: String,
   },
   created() {
-    const url = "http://localhost:8080/PostMainData";
+    const url = "http://192.168.2.147:8080/PostMainData";
    
   
     axios
@@ -1126,7 +1126,7 @@ export default {
     ClickTable: function (tableId) {
       console.log(tableId);
 
-      const url = "http://localhost:8080/DetailMainData";
+      const url = "http://192.168.2.147:8080/DetailMainData";
       axios
         .post(url, {
           DetailId: tableId,
@@ -1410,7 +1410,7 @@ export default {
     },
 
     AxiosUpdate: function (ClassName, UpdateData, UpdateColumn) {
-      const url = "http://localhost:8080/UpdateDetailData/";
+      const url = "http://192.168.2.147:8080/UpdateDetailData/";
 
       axios
         .post(url, {
@@ -1438,7 +1438,7 @@ export default {
     },
     DeletePost: function (PostPaId,key) {
       console.log(PostPaId);
-      const url = "http://localhost:8080/PostDelData";
+      const url = "http://192.168.2.147:8080/PostDelData";
       axios
         .post(url, {
           PaID: PostPaId,
@@ -1460,7 +1460,7 @@ export default {
   CallUpdateMain:function()
 {
   this.ReChangeBool();
-  const url = "http://localhost:8080/PostMainData";
+  const url = "http://192.168.2.147:8080/PostMainData";
   console.log("已刷新");
     
 axios
